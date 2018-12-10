b863588050e0e817a0ec36f11a3e77a500d30659
K_ar_el <verhulstkarel@hotmail.be>
2018-12-10 22:34:41 +0100

    Crouch + animation
    update some animation
    Add crouch code and animation

--- Assets/Scripts/CharacterBehaviour.cs
+++ Assets/Scripts/CharacterBehaviour.cs
@@ -6,43 +6,54 @@ using UnityEngine;
 public class CharacterBehaviour : MonoBehaviour
 {
     //locomotion
-    //[SerializeField]
-    //private float _mass = 75; // [kg]
     [SerializeField]
     private float _acceleration = 3; // [m/s^2]
 
     //dependencies
     [SerializeField]
     private Transform _absoluteForward;
-
     [SerializeField]
     private float _maxRunningSpeed = (30.0f * 1000) / (60 * 60); // [m/s], 30km/h
 
-    private InputController _ic = new InputController();
+    [SerializeField]
+    private float _jumpHeight = 1f;
+    [SerializeField]
+    private float _dragOnGround = 3f;
 
+    private InputController _ic = new InputController();
     private CharacterController _characterController;
+
     private Vector3 _velocity = Vector3.zero;
     private Vector3 _movement;
     private bool _jump;
     private bool _isJumping;
-    [SerializeField]
-    private float _jumpHeight = 1f;
-    
+    private bool _isCrouch;
 
-    [SerializeField]
-    private float _dragOnGround = 3f;
+    float currentHeight;
+    Vector3 currentCenter;
+
+    
 
     //animations
     private Animator _animator;
     private int _horizontalVelocityParam = Animator.StringToHash("HorizontalVelocity");
     private int _verticalVelocityParam = Animator.StringToHash("VerticalVelocity");
     private int _isJumpParam = Animator.StringToHash("IsJumping");
+    private int _isCrouchParam = Animator.StringToHash("IsCrouch");
+
+    //test
+    private HeadTrigger _headTrigger;
 
     // Use this for initialization
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
         _animator = this.GetComponent<Animator>();
+
+        currentHeight = _characterController.height;
+        currentCenter = _characterController.center;
+
+        _headTrigger = FindObjectOfType<HeadTrigger>();
     }
 
     // Update is called once per frame
@@ -51,20 +62,23 @@ public class CharacterBehaviour : MonoBehaviour
         //_movement = _ic.KeyMove();
         _movement = _ic.GetLeftJoystickInput();
                 
-        if (_ic.IsButtonAPressed())
+        if (_ic.IsButtonAPressed() && !_isCrouch)
         {
             _isJumping = true;
         }
 
-        if (_ic.IsLeftJoystickButtonPressed())
+        Debug.Log(_headTrigger.IsInTunnel);
+        if (_ic.IsLeftJoystickButtonPressed() && !_headTrigger.IsInTunnel)
         {
-            Debug.Log("Crouch");
+            _isCrouch = !_isCrouch;
+            _animator.SetBool(_isCrouchParam, _isCrouch);
+            EditCharactControllerParams(_isCrouch);
         }
     }
-
+    
     void FixedUpdate()
     {
-        Debug.Log(_velocity.y);
+        //Debug.Log(_velocity.y);
         ApplyGround();
         ApplyGravity();
         ApplyMovement();
@@ -78,7 +92,6 @@ public class CharacterBehaviour : MonoBehaviour
         JumpAnimation();
 
         DoMovement();
-
     }
 
     private void ApplyGravity()
@@ -125,16 +138,25 @@ public class CharacterBehaviour : MonoBehaviour
             {
                 _velocity = Vector3.zero;
             }
-            if (_movement.magnitude <= 0.6)
+            if (!_isCrouch)
             {
-                _acceleration = 15;
-                _dragOnGround = 10;
+                
+                if (_movement.magnitude <= 0.6)
+                {
+                    _acceleration = 15;
+                    _dragOnGround = 10;
+                }
+                else if (_movement.magnitude <= 0.4)
+                {
+                    _acceleration = 5;
+                }
+                else
+                {
+                    _acceleration = 20;
+                    _dragOnGround = 5;
+                }
             }
-            else
-            {
-                _acceleration = 20;
-                _dragOnGround = 5;
-            }
+            
 
             _velocity = _velocity * (1 - Time.deltaTime * _dragOnGround);
 
@@ -164,8 +186,6 @@ public class CharacterBehaviour : MonoBehaviour
         if (_isJumping && _characterController.isGrounded)
         {
             _jump = true;
-            _characterController.center = new Vector3(0, 1.4f, 0);
-            _characterController.height = 1.4f;
 
             _velocity += -Physics.gravity.normalized * Mathf.Sqrt(2 * Physics.gravity.magnitude * _jumpHeight);
             _isJumping = false;
@@ -173,24 +193,35 @@ public class CharacterBehaviour : MonoBehaviour
         else
         {
             _jump = false;
-            _characterController.center = new Vector3(0, 1, 0);
-            _characterController.height = 2f;
         }
     }
 
     private void MoveAnimation()
     {
-        Vector3 relativeMovement = RelativeDirection(_movement);
-
-        Vector3 localVelocityXZ = gameObject.transform.InverseTransformDirection(relativeMovement);
-        
-        
         _animator.SetFloat(_horizontalVelocityParam, _movement.x);
         _animator.SetFloat(_verticalVelocityParam, _movement.z);
     }
 
-    public void JumpAnimation()
+    private void JumpAnimation()
     {
         _animator.SetBool(_isJumpParam, _jump);
     }
+
+    private void EditCharactControllerParams(bool controlObject)
+    {
+        
+        
+        if (controlObject)
+        {
+            _characterController.height = 1.3f;
+            _characterController.center = new Vector3(0, .7f, 0);
+        }
+        else
+        {
+            _characterController.height = currentHeight;
+            _characterController.center = currentCenter;
+        }
+        
+    }
+    
 }
---
+++ Assets/Scripts/HeadTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadTrigger : MonoBehaviour {
+
+    public bool IsInTunnel { get; set; }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            IsInTunnel = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            Debug.Log("should i stay our should i go");
+            IsInTunnel = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            IsInTunnel = false;
+        }
+    }
+}
