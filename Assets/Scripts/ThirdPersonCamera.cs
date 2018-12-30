510630bd300589d41171355b1787572fcea983b1
K_ar_el <verhulstkarel@hotmail.be>
2018-12-30 19:53:51 +0100

    fix rotation ybot animation

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -50,6 +50,6 @@ public class ThirdPersonCamera : MonoBehaviour {
          *     problem: rotatie animatie gaat veel te traag in vergelijking met de snelheid van de camera
          *              de snelheid van de animatie wordt in de blend tree niet aangepast ook al is de snelheid meer dan 1
          */
-        //_ac.RotateCameraAnimation(_characterController.transform.position.y);
+            _ac.RotateCameraAnimation(_ic.GetRightJoystickInput().x);
     }
 }
