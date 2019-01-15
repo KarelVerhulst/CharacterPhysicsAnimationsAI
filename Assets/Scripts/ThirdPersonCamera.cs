40734ea931a5e02ed7c7a189edc4688fba24bf54
K_ar_el <verhulstkarel@hotmail.be>
2019-01-15 21:40:32 +0100

    testing smoothing camera
    unfortunately doesn't work good

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -13,12 +13,15 @@ public class ThirdPersonCamera : MonoBehaviour {
     //[SerializeField]
     //private float _smooth = 3f;
     [SerializeField]
+    private Transform _standardPos;
+    [SerializeField]
     private float _cameraSpeed = 50f;
     [SerializeField]
     private Transform _characterPos;
     [SerializeField]
+    private GameObject _character;
+    
     private CharacterController _characterController;
-
     private Vector3 _v3Rotate = Vector3.zero;
     private Vector2 _rotationLimit = new Vector2(-20, 40);
     private InputController _ic = InputController.Instance();
@@ -26,19 +29,23 @@ public class ThirdPersonCamera : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        _ac = new AnimationController(this.GetComponentInParent<Animator>());
+        _characterController = _character.GetComponent<CharacterController>();
+        
+        _ac = new AnimationController(_character.GetComponent<Animator>());
     }
 
     // Update is called once per frame
     void Update () {
 
+        //this.transform.position = Vector3.Lerp(this.transform.position, _standardPos.position, Time.deltaTime * _smooth);
+        //this.transform.forward = Vector3.Lerp(this.transform.forward, _standardPos.forward, Time.deltaTime * _smooth);
 
         //rotate camera vertical with a limit
         _v3Rotate.x += _ic.GetRightJoystickInput().y * _cameraSpeed * Time.deltaTime;
         _v3Rotate.x = Mathf.Clamp(_v3Rotate.x, _rotationLimit.x, _rotationLimit.y);
 
         _characterPos.transform.localEulerAngles = _v3Rotate;
-        
+
         if (_ac.CheckIfAnimationIsPlaying(0,"PushAtSwitch") || _ac.CheckIfAnimationIsPlaying(0, "PickUpObject") || _ac.CheckIfAnimationIsPlaying(0, "Blend Tree PushBox") || _ac.CheckIfAnimationIsPlaying(4, "Climb Layer.BlendTreeClimb"))
             return;
 
